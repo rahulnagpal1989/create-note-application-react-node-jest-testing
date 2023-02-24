1b35a5b2d5938080dbc9ee6861de489190cbcc1b
Rahul Nagpal <30521990+rahulnagpal1989@users.noreply.github.com>
2023-02-24 12:04:21 +0530

    Add files via upload

--- nodejs/routes/routes.ts
+++ nodejs/routes/routes.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const {homePage, addNote, deleteNote, getNotes} = require("../controllers/controller.js");
+const {homePage, addNote, deleteNote, getNotes} = require("../controllers/controller");
 
 router.get('/', homePage);
 router.post('/add-note', addNote);
