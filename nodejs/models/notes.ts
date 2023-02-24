989855bb0ad69c9a632559f0e5c571257466e837
Rahul Nagpal <30521990+rahulnagpal1989@users.noreply.github.com>
2023-02-24 12:05:03 +0530

    Add files via upload

--- nodejs/models/notes.ts
+++ nodejs/models/notes.ts
@@ -1,13 +1,13 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize:object, DataTypes:any) => {
 	class Notes extends Model {
 		/**
 		 * Helper method for defining associations.
 		 * This method is not a part of Sequelize lifecycle.
 		 * The `models/index` file will call this method automatically.
 		 */
-		static associate(models) {
+		static associate(models:string) {
 			// define association here
 		}
 	}
